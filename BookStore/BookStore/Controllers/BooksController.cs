b04fb6c0edc46f7169910fee6bd09accd84701c7
Vipin Shukla <vipinshukla5805@gmail.com>
2016-11-28 22:52:11 +0530

    added some more methods in book store

--- BookStore/BookStore/Controllers/BooksController.cs
+++ BookStore/BookStore/Controllers/BooksController.cs
@@ -15,6 +15,7 @@ using BookStore.DTOs;
 
 namespace BookStore.Controllers
 {
+    [RoutePrefix("api/books")]
     public class BooksController : ApiController
     {
         private BookStoreContext db = new BookStoreContext();
@@ -28,17 +29,18 @@ namespace BookStore.Controllers
                 Genre = x.Genre
             };
 
-        // GET: api/Books
+        // GET api/Books
+        [Route("")]
         public IQueryable<BookDto> GetBooks()
         {
-            //return db.Books;
             return db.Books.Include(b => b.Author).Select(AsBookDto);
         }
 
-        // GET: api/Books/5
+        // GET api/Books/5
+        [Route("{id:int}")]
         [ResponseType(typeof(BookDto))]
         public async Task<IHttpActionResult> GetBook(int id)
-        {            
+        {
             BookDto book = await db.Books.Include(b => b.Author)
                 .Where(b => b.BookId == id)
                 .Select(AsBookDto)
@@ -49,6 +51,55 @@ namespace BookStore.Controllers
             }
 
             return Ok(book);
+        }
+
+        [Route("{id:int}/details")]
+        [ResponseType(typeof(BookDetailDto))]
+        public async Task<IHttpActionResult> GetBookDetail(int id)
+        {
+            var book = await (from b in db.Books.Include(b => b.Author)
+                              where b.AuthorId == id
+                              select new BookDetailDto
+                              {
+                                  Title = b.Title,
+                                  Genre = b.Genre,
+                                  PublishDate = b.PublishDate,
+                                  Price = b.Price,
+                                  Description = b.Description,
+                                  Author = b.Author.Name
+                              }).FirstOrDefaultAsync();
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [Route("{genre}")]
+        public IQueryable<BookDto> GetBooksByGenre(string genre)
+        {
+            return db.Books.Include(b => b.Author)
+                .Where(b => b.Genre.Equals(genre, StringComparison.OrdinalIgnoreCase))
+                .Select(AsBookDto);
+        }
+
+        [Route("~api/authors/{authorId}/books")]
+        public IQueryable<BookDto> GetBooksByAuthor(int authorId)
+        {
+            return db.Books.Include(b => b.Author)
+                .Where(b => b.AuthorId == authorId)
+                .Select(AsBookDto);
+        }
+
+        [Route("date/{pubdate:datetime:regex(\\d{4}-\\d{2}-\\d{2})}")]
+        [Route("date/{*pubdate:datetime:regex(\\d{4}/\\d{2}/\\d{2})}")]
+        public IQueryable<BookDto> GetBooks(DateTime pubdate)
+        {
+            return db.Books.Include(b => b.Author)
+                .Where(b => DbFunctions.TruncateTime(b.PublishDate)
+                    == DbFunctions.TruncateTime(pubdate))
+                .Select(AsBookDto);
         }
 
         // PUT: api/Books/5
